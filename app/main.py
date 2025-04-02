9a9ebf04a863640516c768ce5d4f60db204c4204
Chih Kai Wen <ckwen1988@gmail.com>
2025-04-02 15:30:27 +0800

    add cors, log middleware

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 from app.api.v1.routers import shinsas
+from .middlewares import LoggingMiddleware
+
+origins = ["*"]
 
 app = FastAPI(title="Kyudo Tori", version="1.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_middleware(LoggingMiddleware)
 
 @app.get("/")
 def read_root():
